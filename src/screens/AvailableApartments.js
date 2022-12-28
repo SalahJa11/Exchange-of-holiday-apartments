14c3ae1999a81249a4b6c0688b784b71f26daad3
SalahJa11 <salah.jawabreh.8@gmail.com>
2022-12-28 18:54:45 +0200

    28-12

--- src/screens/AvailableApartments.js
+++ src/screens/AvailableApartments.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Background from "../components/Background";
+import MapView from "react-native-maps";
 // import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import BackButton from "../components/BackButton";
 export default function AvailableApartments({ navigation }) {
   return (
@@ -9,7 +10,13 @@ export default function AvailableApartments({ navigation }) {
       <BackButton goBack={navigation.goBack} />
       {/*start your code here*/}
       <Text>its AvailableApartments page start your code here</Text>
+      <MapView style={styles.map} />
     </Background>
   );
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
